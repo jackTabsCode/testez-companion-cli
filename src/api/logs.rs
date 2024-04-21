d6ef24466db7c37ca7cd0c161aa40931bdc05e65
Jack T <jack@jackt.space>
2024-04-21 01:19:48 -0700

    Results are in!

--- src/api/logs.rs
+++ src/api/logs.rs
@@ -1,6 +1,7 @@
 use axum::Json;
 use serde_json::Value;
 
+// Not implemented yet
 pub async fn logs(Json(body): Json<Value>) {
     println!("{}", body)
 }
--- src/api/results.rs
+++ src/api/results.rs
@@ -1,13 +1,42 @@
+use axum::Json;
+use console::style;
+use serde_json::Value;
 use std::process::exit;
 
-use axum::Json;
-use serde_json::Value;
+use crate::testez::{ReporterChildNode, ReporterOutput, ReporterStatus};
 
-use crate::testez::ReporterOutput;
+fn print_children(children: Vec<ReporterChildNode>, indent: usize) {
+    for child in children {
+        let styled_phrase = match child.status {
+            ReporterStatus::Success => style(format!("✓ {}", child.plan_node.phrase)).green(),
+            ReporterStatus::Failure => style(format!("X {}", child.plan_node.phrase)).red(),
+            ReporterStatus::Skipped => style(format!("↪ {}", child.plan_node.phrase)).blue(),
+        };
+        println!("{}{}", " ".repeat(indent), styled_phrase);
+
+        for error in child.errors {
+            // Thanks Copilot!
+            let indented_error: String = error.split('\n').fold(String::new(), |mut acc, line| {
+                acc.push_str(&format!("{}{}\n", " ".repeat(indent + 2), line));
+                acc
+            });
+            print!("{}", indented_error);
+        }
+
+        print_children(child.children, indent + 2);
+    }
+}
 
 pub async fn results(Json(body): Json<Value>) {
     let output: ReporterOutput = serde_json::from_value(body).unwrap();
-    println!("{:#?}", output);
+
+    print_children(output.children, 0);
+
+    println!();
+
+    println!("{} {}", style("✓ Success:").green(), output.success_count);
+    println!("{} {}", style("X Failure:").red(), output.failure_count);
+    println!("{} {}", style("↪ Skip:").blue(), output.skipped_count);
 
     exit(0);
 }
